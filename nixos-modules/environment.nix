a974acbe944fd5e902e450e27cc36559dfc4cc12
rob <corestepper@gmail.com>
2024-11-11 11:18:41 -0600

    Cleaning up things

--- nixos-modules/environment.nix
+++ nixos-modules/environment.nix
@@ -7,7 +7,7 @@ let
     QT_QPA_PLATFORM = "wayland";
     MOZ_ENABLE_WAYLAND = "1";
   };
-  # Custom font
+  # Custom font for additional use
   customFont = pkgs.fetchurl {
     url = "https://downloads.sourceforge.net/project/corefonts/the%20fonts/final/times32.exe";
     sha256 = "1aq7z3l46vwgqljvq9zfgkii6aivy00z1529qbjkspggqrg5jmnv";
@@ -38,54 +38,25 @@ in
     XDG_SESSION_TYPE = "wayland";
     XDG_ICON_THEME = "breeze";
     LIBVA_DRIVER_NAME = "iHD";  # Intel VAAPI driver for newer Intel GPUs
-    LIBVA_DRIVERS_PATH = "/nix/store/3rihr4l4xlggcczsbzqcjm1mmpvlzvq8-intel-media-driver-24.2.1/lib/dri";  # Exact path to iHD driver
+    LIBVA_DRIVERS_PATH = "/nix/store/3rihr4l4xlggcczsbzqcjm1mmpvlzvq8-intel-media-driver-24.2.1/lib/dri";  # Path to iHD driver
   };
 
-  # Essential system packages, grouped by category
+  # System packages
   environment.systemPackages = with pkgs; [
-    # Development Tools
     cmake gnumake git emacs tmux konsole docker docker-compose preload
-
-    # Fonts
     dejavu_fonts fira-code nerdfonts meslo-lgs-nf jetbrains-mono open-sans source-code-pro
-
-    # System Utilities
     brightnessctl bubblewrap bleachbit fd htop lm_sensors shellcheck starship ispell
-
-    # Wayland Tools
-    swaybg waybar wayland-protocols wayland-utils wl-clipboard xwayland hyprland hyprpaper hyprcursor
-
-    # Multimedia
-    vlc mpv 
-
-    # Networking
+    swaybg waybar wayland-protocols wayland-utils wl-clipboard hyprland hyprpaper hyprcursor
+    vlc mpv udevil udisks2
     tor tor-browser-bundle-bin wireguard-tools networkmanager networkmanagerapplet
-
-    # Miscellaneous
     firefox-wayland flatpak keepassxc kdeconnect kwin okular pavucontrol proxychains-ng pulseaudio
-    python3 python311Packages.pip python312Packages.pip ripgrep sddm sddm-chili-theme
-    sudo texliveFull tlp unzip usbutils webcamoid wget wineWow64Packages.fonts
+    python3 python311Packages.pip python312Packages.pip ripgrep sudo texliveFull unzip usbutils webcamoid wget wineWow64Packages.fonts
     xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xdg-utils xfce.thunar xorg.xf86inputsynaptics
     xorg.xrandr xorg.xdpyinfo zoom-us
-
-    # Icons and Themes
     kdePackages.breeze-icons papirus-icon-theme material-design-icons kdePackages.qtstyleplugin-kvantum
     libsForQt5.qt5ct libsForQt5.bismuth
-
-    # VAAPI for Intel GPU hardware acceleration
-    intel-media-driver  # Intel VAAPI driver for video decoding/encoding
-    libva-utils         # Utility for VAAPI diagnostics
-    mesa                # Required for OpenGL acceleration
-    libva               # VAAPI library for video acceleration
-    libdrm              # Direct Rendering Manager
-    intel-vaapi-driver
-
-    # Vulkan for 3D acceleration (optional, but recommended for gaming and multimedia)
-    vulkan-loader
-    vulkan-tools
-    mesa.drivers        # Corrected package for Intel Vulkan drivers
-
-    # Nonfree firmware
+    intel-media-driver libva-utils mesa libva libdrm intel-vaapi-driver
+    vulkan-loader vulkan-tools mesa.drivers
     firmwareLinuxNonfree
   ];
 
@@ -108,16 +79,35 @@ in
   services.flatpak.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Automount USB drives with a custom service
+  services.udisks2.enable = true;
+  systemd.user.services.automount-usb = {
+    description = "Automount USB drives";
+    serviceConfig = {
+      ExecStart = "${pkgs.udiskie}/bin/udiskie --no-tray --automount";
+    };
+    wantedBy = [ "default.target" ];
+  };
+
+  # Configure SDDM and apply a theme
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.theme = "chili";  # Replace 'chili' with your desired theme
+
   system.stateVersion = "24.05";
 
-  # Overlay for enabling Waybar experimental features
+  # Overlay to remove duplicate files from Papirus icon theme to avoid collisions
   nixpkgs.overlays = [
     (self: super: {
-      waybar = super.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      papirus-icon-theme = super.papirus-icon-theme.overrideAttrs (oldAttrs: {
+        postInstall = ''
+          # Remove conflicting icons shared with breeze-icons
+          rm -rf $out/share/icons/breeze/devices/22@3x/*.svg
+          rm -rf $out/share/icons/breeze/index.theme
+        '';
       });
     })
   ];
+
   virtualisation.docker.enable = true;
 
   # Allow unfree packages if necessary
